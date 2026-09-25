88cb035ea992a89dd5f62547b2096d475095dfa6
Sublicuentas <marvinvalenzuela293@gmail.com>
2026-09-25 17:39:25 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sublicuentas-socios-web-brief-20260925-2-mobilefix';
+const CACHE='sublicuentas-socios-web-brief-20260925-1';
 const SHELL=['./','./index.html','./assets/app.css','./assets/panel-web-20260925.css','./assets/core.js','./assets/operations.js','./assets/catalog-aula.js','./assets/panel-web-20260925.js','./assets/messaging.js','./assets/robot-socios.webp','./assets/icon-192.png','./assets/icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
